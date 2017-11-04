685c75084121b3d70695dcc33e8b0727c543828f
zjuchenyuan <chenyuan@zju.edu.cn>
2017-11-04 09:26:55 +0800

    [fix] one.zju change parameter formStepId to stepId

--- zjuactivity.py
+++ zjuactivity.py
@@ -21,11 +21,11 @@ class ZJUActivity(ZJUAUTHME):
     def _get_xiaoqu(self, csrf_token=None):
         """
         csrf_token为空时返回缓存数据，否则发起Post请求查询
-        其中formStepId只需指定一个"报告会、论坛、讲座活动审批"的流程ID即可，无需是自己创建的
+        其中stepId只需指定一个"报告会、论坛、讲座活动审批"的流程ID即可，无需是自己创建的
         """
         if csrf_token is None:
             return self.CACHE["xiaoqu"]
-        xiaoqu_r = self.a.post_dict(self.DOMAIN+"/infoplus/interface/suggest",{"formStepId":"408560","prefix":"","type":"Code","code":"xiaoQu","parent":"","isTopLevel":"true","pageNo":"0","rand":"","settings":"{}","csrfToken":csrf_token,"lang":"en"})
+        xiaoqu_r = self.a.post_dict(self.DOMAIN+"/infoplus/interface/suggest",{"stepId":"408560","prefix":"","type":"Code","code":"xiaoQu","parent":"","isTopLevel":"true","pageNo":"0","rand":"","settings":"{}","csrfToken":csrf_token,"lang":"en"})
         data = [i["codeId"] for i in xiaoqu_r.json()["items"]]
         return data
 
@@ -33,11 +33,11 @@ class ZJUActivity(ZJUAUTHME):
         """
         输入校区，查询校区包含的楼宇名称
         csrf_token为空时返回缓存数据，否则发起Post请求查询
-        其中formStepId只需指定一个"报告会、论坛、讲座活动审批"的流程ID即可，无需是自己创建的
+        其中stepId只需指定一个"报告会、论坛、讲座活动审批"的流程ID即可，无需是自己创建的
         """
         if csrf_token is None:
             return self.CACHE["xiaoqu_louyu"][xiaoqu]
-        louyu_r = self.a.post_dict(self.DOMAIN+"/infoplus/interface/suggest",{"formStepId":"408560","prefix":"","type":"Code","code":"LouYu","parent":xiaoqu,"isTopLevel":"false","pageNo":"0","rand":"","settings":"{}","csrfToken":csrf_token,"lang":"en"})
+        louyu_r = self.a.post_dict(self.DOMAIN+"/infoplus/interface/suggest",{"stepId":"408560","prefix":"","type":"Code","code":"LouYu","parent":xiaoqu,"isTopLevel":"false","pageNo":"0","rand":"","settings":"{}","csrfToken":csrf_token,"lang":"en"})
         data = [i["codeId"] for i in louyu_r.json()["items"]]
         return data
 
@@ -45,11 +45,11 @@ class ZJUActivity(ZJUAUTHME):
         """
         输入楼宇，查询该楼宇包含的资源
         csrf_token为空时返回缓存数据，否则发起Post请求查询
-        其中formStepId只需指定一个"报告会、论坛、讲座活动审批"的流程ID即可，无需是自己创建的
+        其中stepId只需指定一个"报告会、论坛、讲座活动审批"的流程ID即可，无需是自己创建的
         """
         if csrf_token is None:
             return self.CACHE["louyu_ziyuan"][louyu]
-        zy_r = self.a.post_dict(self.DOMAIN+"/infoplus/interface/suggest",{"formStepId":"408560","prefix":"","type":"Code","code":"ZY","parent":louyu,"isTopLevel":"false","pageNo":"0","rand":"","settings":"{}","csrfToken":csrf_token,"lang":"en"})
+        zy_r = self.a.post_dict(self.DOMAIN+"/infoplus/interface/suggest",{"stepId":"408560","prefix":"","type":"Code","code":"ZY","parent":louyu,"isTopLevel":"false","pageNo":"0","rand":"","settings":"{}","csrfToken":csrf_token,"lang":"en"})
         data = [(i["codeId"], i["codeName"]) for i in zy_r.json()["items"]]
         return data
 
@@ -59,7 +59,7 @@ class ZJUActivity(ZJUAUTHME):
         此函数用于实时查询，不应该在此层面配置缓存，csrf_token为必须参数
         虽然浏览器发出了60多个参数，但经过我的层层筛选 只有楼宇id是需要的
         """
-        query_r = self.a.post(self.DOMAIN+"/infoplus/interface/fieldChanging","formStepId=408560&formData=%7B%22fieldSQCDZY%22%3A%22{ziyuan_id}%22%7D&fieldName=fieldSQCDbtnQuery&fieldValue=&path=&csrfToken={csrf_token}".format(**locals()))
+        query_r = self.a.post(self.DOMAIN+"/infoplus/interface/fieldChanging","stepId=408560&formData=%7B%22fieldSQCDZY%22%3A%22{ziyuan_id}%22%7D&fieldName=fieldSQCDbtnQuery&fieldValue=&path=&csrfToken={csrf_token}".format(**locals()))
         occupy_data = json.loads(query_r.json()["entities"][0])["occupyZY"]
         result = [] # 格式： [校区,楼宇,资源,资源id,开始时间,结束时间,资源使用情况(活动名称)]
         for item in occupy_data:
